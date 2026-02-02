37e2c92395ab005a3330be232db1749674086fe3
Mateusz Ogonek <ogonekmateusz69@gmail.com>
2026-02-02 16:24:29 +0100

    import logo to navbar

---
+++ src/sections/navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import cakeLogo from '../assets/cake-logo.png'
+
+export default function Navbar() {
+  return (
+    <header>
+        <nav>
+            
+        </nav>
+    </header>
+  )
+}
