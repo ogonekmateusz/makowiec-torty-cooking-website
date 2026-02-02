63baec94523800fd501aaa9fd6b6dcc64e615a29
Mateusz Ogonek <ogonekmateusz69@gmail.com>
2026-02-02 16:26:20 +0100

    add logo section structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import HeroLayout from './layouts/hero-layout'
+import Navbar from './sections/navbar'
 
 export default function App() {
   return (
     <HeroLayout>
-      <h1 className="text-4xl font-bold text-center">Welcome to My App</h1>
-      <p className="text-center mt-4">This is a simple hero layout example.</p>
+      <Navbar />
     </HeroLayout>
   )
 }
--- src/sections/navbar.jsx
+++ src/sections/navbar.jsx
@@ -1,12 +1,15 @@
-import React from 'react'
-import cakeLogo from '../assets/cake-logo.png'
+import React from "react";
+import cakeLogo from "../assets/cake-logo.png";
 
 export default function Navbar() {
   return (
     <header>
-        <nav>
-            
-        </nav>
+      <nav>
+        <div className="logo-section">
+          <img src={cakeLogo} alt="logo" className="w-10" />
+          <h1 className="text-2xl font-bold">MAKOWIEC TORTY</h1>
+        </div>
+      </nav>
     </header>
-  )
+  );
 }
