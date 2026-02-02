546b44c3c4696f0c3ba2937d8b8aaf3af407c085
Mateusz Ogonek <ogonekmateusz69@gmail.com>
2026-02-02 16:40:04 +0100

    change direct of gradient

--- src/layouts/hero-layout.jsx
+++ src/layouts/hero-layout.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function HeroLayout({ children }) {
   return (
-    <div className="hero-layout w-full bg-linear-to-r from-pink-300 via-pink-400 to-pink-500">
+    <div className="hero-layout w-full bg-linear-to-tr from-pink-300 via-pink-400 to-pink-500">
       {children}
     </div>
   )
