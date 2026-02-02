22fbe4442ff6bf69fe5c52af453bb2d871f1e56a
Mateusz Ogonek <ogonekmateusz69@gmail.com>
2026-02-02 16:27:46 +0100

    add menu section structure

--- src/sections/navbar.jsx
+++ src/sections/navbar.jsx
@@ -9,6 +9,23 @@ export default function Navbar() {
           <img src={cakeLogo} alt="logo" className="w-10" />
           <h1 className="text-2xl font-bold">MAKOWIEC TORTY</h1>
         </div>
+        <div className="menu-section">
+            <ul>
+                <li>
+                    <a href="#">Strona główna</a>
+                </li>
+                <li>
+                    <a href="#">Oferta</a>
+                </li>
+                <li>
+                    <a href="#">Galeria</a>
+                </li>
+                <li>
+                    <a href="#">Kontakt</a>
+                </li>
+            </ul>
+        </div>
+        
       </nav>
     </header>
   );
