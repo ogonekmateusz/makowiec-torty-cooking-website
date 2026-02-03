94e65327bb0a9c50a38991eba0ab4c0176d271f5
Mateusz Ogonek <ogonekmateusz69@gmail.com>
2026-02-03 11:30:36 +0100

    import cake particles to project

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import HeroLayout from './layouts/hero-layout'
 import Navbar from './sections/navbar'
 import Hero from './sections/hero'
+import cakeParticles from './assets/cake-particles.png'
 
 export default function App() {
   return (
